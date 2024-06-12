c0bebd478e9cd7c28d26ad9655a8b88ad5b285e5
Matt <matt@allty.com>
2024-06-12 07:37:19 +0200

    clean code

--- packages/template-example/src/index.js
+++ packages/template-example/src/index.js
@@ -1,3 +1,5 @@
 // You can put your javascript here.
 // This script is loaded as a module, so it is defered,
 // allowing you to immediately manipulate the DOM.
+
+console.log("Hello from the template-example package!");
