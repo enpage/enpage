d34d7233b75ea86d5744d0cbe26fc7a36f0adabe
Matt <matt@allty.com>
2024-06-22 21:19:10 +0200

    Refactor template example

--- packages/template-example/postcss.config.js
+++ packages/template-example/postcss.config.js
@@ -1,14 +1,14 @@
 // @ts-nocheck
 //-----------------------------------------------------------------------------------------------
 //
-// You should not need to edit this file,
+// You may want to customize the PostCSS configuration for your template here.
 //
 //-----------------------------------------------------------------------------------------------
 export default {
   plugins: {
     "postcss-import": {},
     "postcss-preset-env": {},
-    tailwindcss: {},
+    ...(!!process.env.DISABLE_TAILWIND ? {} : { tailwindcss: {} }),
     ...(process.env.NODE_ENV === "production"
       ? {
           cssnano: {},
