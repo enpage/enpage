1af20e7fe4046705cf63c2aa62f496e2d3f62c75
Matthias Etienne <matt@allty.com>
2024-07-15 13:33:01 +0200

    add postinstall script

---
+++ scripts/postinstall.cjs
@@ -0,0 +1,16 @@
+const fs = require("node:fs");
+const { resolve } = require("node:path");
+
+const cliFile = resolve(__dirname, "../packages/sdk/dist/node/cli/program.js");
+
+const getPackageManager = () => (process.env.npm_config_user_agent ?? "").split("/")[0] || "npm";
+
+if (!fs.existsSync(cliFile)) {
+  console.log("Building SDK CLI...");
+  const manager = getPackageManager();
+  // run build script in the package directory packages/sdk
+  require("node:child_process").execSync(`${manager} build`, {
+    cwd: resolve(__dirname, "../packages/sdk"),
+    stdio: "inherit",
+  });
+}
