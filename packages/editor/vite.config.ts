2732fb58b4bef58c1bc6dbdc7d37163b7b179f32
Matt <matt@allty.com>
2024-08-01 13:51:17 +0200

    rework editor vite config

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -9,7 +9,7 @@ const [, port] = process.env.ENPAGE_SITE_HOST.split(":");
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: ["PUBLIC_"],
-  plugins: [Inspect(), react()],
+  plugins: [Inspect(), react(), dts()],
   server: {
     port: 3008,
   },
@@ -17,9 +17,32 @@ export default defineConfig({
     copyPublicDir: false,
     lib: {
       entry: {
-        components: "src/components/index.tsx",
+        main: "src/components/index.tsx",
       },
       formats: ["es"],
+    },
+    rollupOptions: {
+      external: [
+        "react-icons",
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        "zustand",
+        "zundo",
+        "clsx",
+        "tailwind-merge",
+        "@uidotdev/usehooks",
+        "@headlessui/react",
+        "@enpage/sdk",
+        "immer",
+      ],
+      output: {
+        globals: {
+          react: "react",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
     },
   },
 });
