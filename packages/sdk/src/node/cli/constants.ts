559d8ef76a7c444de5ce013ef056e7120fc43c03
Matt <matt@allty.com>
2024-08-28 08:26:57 +0200

    add oauth test client id

--- packages/sdk/src/node/cli/constants.ts
+++ packages/sdk/src/node/cli/constants.ts
@@ -1,7 +1,7 @@
 export const CLI_PROJECT_NAME = "enpage-cli";
 export const CONF_USER_TOKEN_KEY = "token";
 export const CLI_LOGIN_POLL_INTERVAL = 3000; // seconds
-export const CLI_OAUTH_CLIENT_ID =
+export const CLI_LOGIN_CLIENT_ID =
   process.env.PUBLIC_ENPAGE_OAUTH_CLIENT_ID ?? "50000000-0000-0000-0000-000000000001";
 export const CLI_LOGIN_POLL_MAX_TIME = 3; // minutes
 export const CLI_LOGIN_POLL_MAX_TRIES = (CLI_LOGIN_POLL_MAX_TIME * 60 * 1000) / CLI_LOGIN_POLL_INTERVAL;
