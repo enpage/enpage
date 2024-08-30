e760b2891e3d0ab1532c1f67a9b4d96247e45bed
Matt <matt@allty.com>
2024-08-31 00:58:32 +0200

    export all samples

---
+++ packages/sdk/src/shared/datasources/samples.ts
@@ -0,0 +1,17 @@
+import { sample as facebookListSample } from "./facebook/posts/sample";
+import { sample as instagramFeedSample } from "./instagram/feed/sample";
+import { sample as mastodonStatusSample } from "./mastodon/status/sample";
+import { sample as rssSample } from "./rss/sample";
+import { sample as threadsMediaSample } from "./threads/media/sample";
+import { sample as tiktokVideoSample } from "./tiktok/video/sample";
+import { sample as youtubeListSample } from "./youtube/list/sample";
+
+export const samples = {
+  "facebook-posts": facebookListSample,
+  "instagram-feed": instagramFeedSample,
+  "mastodon-status": mastodonStatusSample,
+  rss: rssSample,
+  "threads-media": threadsMediaSample,
+  "tiktok-video": tiktokVideoSample,
+  "youtube-list": youtubeListSample,
+} as const;
