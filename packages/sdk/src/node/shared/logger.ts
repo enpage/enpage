0dd238974b491100a0118aeed0a16f2ab43abcfa
Matt <matt@allty.com>
2024-08-31 11:24:46 +0200

    augment logger

--- packages/sdk/src/node/shared/logger.ts
+++ packages/sdk/src/node/shared/logger.ts
@@ -1,4 +1,9 @@
-import { createLogger as createLoggerBase, type LogLevel } from "vite";
+import {
+  createLogger as createLoggerBase,
+  type LogLevel,
+  type LogOptions,
+  type Logger as BaseLogger,
+} from "vite";
 import { version } from "../../../package.json";
 import chalk from "chalk";
 
@@ -12,7 +17,16 @@ export function createLogger(level?: LogLevel, allowClearScreen?: boolean, showV
     logger.info(chalk.hex("#7270c6").bold(`🚀 Enpage v${version}\n`));
   }
 
+  Object.assign(logger, {
+    success: (message: string, options: LogOptions) => logger.info(chalk.green(message), options),
+    error: (message: string, options: LogOptions) => logger.info(chalk.red(message), options),
+    warn: (message: string, options: LogOptions) => logger.info(chalk.yellow(message), options),
+  });
+
   return logger;
 }
 
 export const logger = createLogger();
+export type Logger = BaseLogger & {
+  success: (message: string, options?: LogOptions) => void;
+};
