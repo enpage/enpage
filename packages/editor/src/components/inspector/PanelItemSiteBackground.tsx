a41a1cc5c1718ae05abd89329ff8d1200fa20036
Matt <matt@allty.com>
2024-09-01 15:56:28 +0200

    use `biome ci` in CI

--- packages/editor/src/components/inspector/PanelItemSiteBackground.tsx
+++ packages/editor/src/components/inspector/PanelItemSiteBackground.tsx
@@ -1,6 +1,8 @@
 import { PanelItem, type PanelItemProps } from "./PanelItem";
 
 export function PanelItemSiteBackground({ element }: Omit<PanelItemProps, "title">) {
+  // trying biome in CI
+  eval("console.log('biome')");
   return (
     <PanelItem title="Site background" element={element}>
       Site background
