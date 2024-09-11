62df98abd1c80f5827efdcbfdc1195a7a92beaed
Matt <matt@allty.com>
2024-09-11 15:35:02 +0200

    replace rss-parser by htmlparser2 and add tests

--- packages/sdk/src/shared/datasources/rss/schema.ts
+++ packages/sdk/src/shared/datasources/rss/schema.ts
@@ -3,13 +3,15 @@ import { Type, type Static } from "@sinclair/typebox";
 export const rssSchema = Type.Object({
   title: Type.String(),
   link: Type.String(),
-  description: Type.String(),
+  description: Type.Optional(Type.String()),
+  updated: Type.Optional(Type.String()),
   items: Type.Array(
     Type.Object({
-      title: Type.String(),
-      link: Type.String(),
-      pubDate: Type.String(),
-      content: Type.String(),
+      title: Type.Optional(Type.String()),
+      link: Type.Optional(Type.String()),
+      creator: Type.Optional(Type.String()),
+      content: Type.Optional(Type.String()),
+      pubDate: Type.Optional(Type.String()),
     }),
   ),
 });
