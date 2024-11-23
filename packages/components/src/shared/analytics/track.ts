6beeeb1d06cb9f030c511d87e9dbb75c676a69ea
Matt <matt@allty.com>
2024-11-24 00:05:25 +0100

    remove init in track()

--- packages/components/src/shared/analytics/track.ts
+++ packages/components/src/shared/analytics/track.ts
@@ -1,7 +1,5 @@
 import type { BeaconPayload } from "./types";
 
-window.upAnalyticsQueue = window.upAnalyticsQueue || [];
-
 export function track(type: BeaconPayload["e"], data: BeaconPayload["d"] = {}) {
   const payload: BeaconPayload = {
     e: type,
