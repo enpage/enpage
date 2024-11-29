d6b498228fd3fb558a7452c6652b2728b4a61418
Matt <matt@allty.com>
2024-11-29 08:11:40 +0100

    simplify types (#245)

---
+++ .changeset/tasty-eels-retire.md
@@ -0,0 +1,6 @@
+---
+"@upstart.gg/components": patch
+"@upstart.gg/sdk": patch
+---
+
+Simplify types
--- packages/components/src/editor/demo/App.tsx
+++ packages/components/src/editor/demo/App.tsx
@@ -1,21 +1,14 @@
-import { createSiteConfigFromTemplateConfig, type GenericPageConfig } from "@upstart.gg/sdk/shared/page";
+import { getNewSiteConfig, type GenericPageConfig } from "@upstart.gg/sdk/shared/page";
 import * as testEnpageConfig from "~/test-config";
 import { EditorWrapper } from "~/editor/components/EditorWrapper";
 import { ClientOnly } from "~/shared/utils/client-only";
 import Editor from "~/editor/components/Editor";
 
 export default function App() {
-  const siteConfig = createSiteConfigFromTemplateConfig(testEnpageConfig);
-  const pageConfig: GenericPageConfig = {
-    ...siteConfig,
-    id: crypto.randomUUID(),
-    siteId: crypto.randomUUID(),
-    label: "My site",
-    hostname: "localhost",
-  };
+  const siteConfig = getNewSiteConfig(testEnpageConfig);
   return (
     <ClientOnly>
-      <EditorWrapper pageConfig={pageConfig} mode="local">
+      <EditorWrapper pageConfig={siteConfig.pages[0]} mode="local">
         <Editor />
       </EditorWrapper>
     </ClientOnly>
